29b6e5dc5761623f1ec4dcd205f3123e61672690
Prashu2181 <prasannamadireddy466@gmail.com>
2025-02-07 09:39:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-}
+ let str = s1.toLowerCase();
+    let subStr = s2.toLowerCase();
+    
+    if (subStr.length === 0) return 0; 
+
+    for (let i = 0; i <= str.length - subStr.length; i++) {
+        if (str.slice(i, i + subStr.length) === subStr) {
+            return i;
+        }
+    }
+
+    return -1;
+} 
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
